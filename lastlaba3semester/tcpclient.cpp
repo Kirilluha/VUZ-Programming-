2f3be7641e9f1ba70bf54cd89ea61ad029156f6d
Kirilluha <kirilluhalov@gmail.com>
2024-12-15 19:51:47 +0300

    Fix TCPs

--- lastlaba3semester/tcpclient.cpp
+++ lastlaba3semester/tcpclient.cpp
@@ -2,19 +2,22 @@
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
 
-using boost::asio::ip::tcp;
+using boost::asio::ip::tcp; //Упрощаю доступ к функциям TCP
 
 int main() {
     try {
-        boost::asio::io_service io_service;
+        boost::asio::io_service io_service; //Запуск асинхронных операций
         tcp::resolver resolver(io_service);
-        tcp::resolver::query query("127.0.0.1", "12345");
+        tcp::resolver::query query("127.0.0.1", "12345"); 
+        //Указываю имя и порт (в моем случае localhost и порт 12345)
         tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+            /*resolve выполняет разрешение доменного имени в IP-адрес, 
+        а значит его вообще не обязательно использовать,
+        но пусть будет*/
+        tcp::socket socket(io_service); //Создаю TCP-сокет
+        boost::asio::connect(socket, endpoint_iterator); //Подключаемся к серверу
 
-        tcp::socket socket(io_service);
-        boost::asio::connect(socket, endpoint_iterator);
-
-        std::cout << "Connected to server. Type your message (type 'exit' to quit):\n";
+        std::cout << "Connected to server. Type your message (type 'exit' to quit):\n"; //Отладочное сообщение
 
         while (true) {
             // Ввод сообщения с клавиатуры
@@ -22,28 +25,32 @@ int main() {
             std::cout << "Enter message: ";
             std::getline(std::cin, message);
 
-            // Условие для выхода
+            // Условие для закрытия клиента
             if (message == "exit") {
                 std::cout << "Closing connection.\n";
                 break;
             }
 
-            // Отправка сообщения на сервер
+            //Отправка сообщения на сервер
             boost::asio::write(socket, boost::asio::buffer(message));
 
-            // Буфер для ответа сервера
+            //Буфер для ответа сервера
             char reply[1024];
-            boost::system::error_code error;
-            size_t reply_length = socket.read_some(boost::asio::buffer(reply), error);
+            boost::system::error_code error; //Инициализирую error
 
-            if (!error) {
+            size_t reply_length = socket.read_some(boost::asio::buffer(reply), error); 
+            /*Читаю кол-во байтов ответа сервера и ошибки если она возникла*/
+
+            if (!error) { //Нет ошибки - выведется ответ
                 std::cout << "Reply from server: " << std::string(reply, reply_length) << "\n";
-            } else {
+            } 
+            else { //Есть ошибка - выведется что за ошибка и цикл завершится и все переподключится
                 std::cerr << "Error receiving reply: " << error.message() << "\n";
                 break;
             }
         }
-    } catch (std::exception& e) {
+    } 
+    catch (std::exception& e) { //Обработка исключений
         std::cerr << "Exception: " << e.what() << "\n";
     }
     return 0;
--- lastlaba3semester/tcpserver.cpp
+++ lastlaba3semester/tcpserver.cpp
@@ -2,22 +2,26 @@
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
 
-using boost::asio::ip::tcp;
+using boost::asio::ip::tcp;     //Упрощаю доступ к функциям TCP
 
+//Базовый класс сервера
 class TCPServer {
 public:
+    //Создаю конструктор, принимающий io_service и порт, на котором будет работать сервер
     TCPServer(boost::asio::io_service& io_service, unsigned short port)
-        : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-          socket_(io_service) {
-        start_accept();
+        : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),     //Acceptor для прослушивания
+          socket_(io_service) {     // Сокет для общения с клиентом
+        start_accept();     // Ожидание подключений
     }
 
 private:
+    //Ожидание новых подключений
     void start_accept() {
-        acceptor_.async_accept(socket_,
-            boost::bind(&TCPServer::handle_accept, this, boost::asio::placeholders::error));
+        acceptor_.async_accept(socket_,     //Асинхронно ждем подключения
+            boost::bind(&TCPServer::handle_accept, 
+            this, boost::asio::placeholders::error)); //Обработчик при успешном подключении
     }
-
+    //Обработчик подключения
     void handle_accept(const boost::system::error_code& error) {
         if (!error) {
             std::cout << "Client connected.\n";
@@ -25,24 +29,26 @@ private:
         }
         start_accept(); // Ожидание новых подключений
     }
-
+    //Начинаем читать сообщения
     void start_read() {
-        // Буфер для хранения данных
+        /*Строка кода создаёт буфер для данных, вмещающий до 1024 байтов, 
+        и управляет им с помощью make_shared, который гарантирует освобождение 
+        памяти.*/
         auto buffer = std::make_shared<std::array<char, 1024>>();
 
-        // Асинхронное чтение данных
+        //Асинхронное чтение данных
         socket_.async_read_some(boost::asio::buffer(*buffer),
             [this, buffer](const boost::system::error_code& error, std::size_t bytes_transferred) {
                 if (!error) {
-                    // Сохранение и вывод полученного сообщения
-                    std::string message_received(buffer->data(), bytes_transferred);
+                    //Сохранение и вывод полученного сообщения
+                    std::string message_received(buffer->data(), bytes_transferred); //Конвертирую данные в строку
                     std::cout << "Message received: " << message_received << std::endl;
 
                     // Ответ клиенту
                     auto response = std::make_shared<std::string>(message_received);
                     boost::asio::async_write(socket_, boost::asio::buffer(*response),
                         [this](const boost::system::error_code&, std::size_t) {
-                            // После отправки ответа снова читаем данные
+                            // После отправки ответа снова читаю данные
                             start_read();
                         });
                 } else {
@@ -51,17 +57,16 @@ private:
             });
     }
 
-    tcp::acceptor acceptor_;
+    tcp::acceptor acceptor_; //Чтобы проще вызвать в main()
     tcp::socket socket_;
 };
 
 int main() {
     try {
-        boost::asio::io_service io_service;
-        TCPServer server(io_service, 12345);
-        io_service.run();
-    } catch (std::exception& e) {
+        boost::asio::io_service io_service;                   //Объект для управления асинхронными операциями
+        TCPServer server(io_service, 12345);                  //Создаю сервер на порту 12345
+        io_service.run();                                     //Обработка асинхронных операций
+    } catch (std::exception& e) {                             //Обработка исключений
         std::cerr << "Exception: " << e.what() << "\n";
     }
-    return 0;
 }
